6bf98f2e36741aa53eefed9db4bae0db6a85ff53
EmmVega <emmanuelvegamecinas@gmail.com>
2025-03-31 22:05:45 -0600

    infinite scroll and pulltorefresh

--- presentation/products/components/ProductList.tsx
+++ presentation/products/components/ProductList.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from 'react';
 import { Product } from '@/core/products/interface/product.interface';
-import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, RefreshControl, StyleSheet } from 'react-native';
 import { ProductCard } from './ProductCard';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface Props {
     products: Product[];
@@ -9,12 +10,29 @@ interface Props {
 }
 
 const ProductList = ({products, loadNextPage}: Props) => {
+    const [isRefreshing, setIsRefreshing] = useState(false);
+    const queryClient = useQueryClient();
+    const onPullToRefresh = async () => {
+        setIsRefreshing(true);
+        await new Promise((resolve) => setTimeout(resolve, 200))
+
+        queryClient.invalidateQueries({
+            queryKey: ["products", "infinite"]
+        });
+
+        setIsRefreshing(false);
+    }
     return (
         <FlatList 
             data={products}
             numColumns={2}
             keyExtractor={(item) => item.id}
             renderItem={({item}) => <ProductCard product={item} />}
+            onEndReached={loadNextPage}
+            onEndReachedThreshold={0.8}
+
+            showsVerticalScrollIndicator={false}
+            refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />}
         />
     );
 }
