9be82954dff77a44e912d6e16507d9ac506f2b80
EmmVega <emmanuelvegamecinas@gmail.com>
2025-04-11 22:14:51 -0600

    all buttons on camera

--- app/camera/index.tsx
+++ app/camera/index.tsx
@@ -1,15 +1,31 @@
 import { ThemedText } from '@/presentation/theme/components/ThemedText';
+import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor';
+import { Ionicons } from '@expo/vector-icons';
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { router } from 'expo-router';
+import { useRef, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native';
 
 export default function CameraScreen() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+  
+  const cameraRef = useRef<CameraView>(null);
 
   if (!permission) {
     // Camera permissions are still loading.
     return <View />;
+  }
+
+  const onShutterButtonPress = async () => {
+    if(!cameraRef.current) return;
+
+    const picture = await cameraRef.current.takePictureAsync({
+        quality: 0.7,
+    })
+
+    console.log(picture);
+    if(!picture?.uri) return;
   }
 
   if (!permission.granted) {
@@ -23,9 +39,13 @@ export default function CameraScreen() {
             Solicitar Permiso
             </ThemedText>
         </TouchableOpacity>
-        
+
       </View>
     );
+  }
+
+  const onReturnCancel = () => {
+    router.dismiss();
   }
 
   function toggleCameraFacing() {
@@ -34,15 +54,63 @@ export default function CameraScreen() {
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+            <ShutterButton onPress={onShutterButtonPress}/>
+            <FlipCameraButton onPress={toggleCameraFacing}/>
+            <GalleryButton />
+            <ReturnCancelButton onPress={onReturnCancel}/>
+          {/* <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
+          </TouchableOpacity> */}
       </CameraView>
     </View>
   );
+}
+
+//custom components
+const ShutterButton = ({onPress = () => {}}) => {
+    const dimensions = useWindowDimensions();
+    const primaryColor = useThemeColor({}, 'primary')
+    return (
+        <TouchableOpacity
+        onPress={onPress}
+            style={[
+                styles.shutterButton,
+                {
+                    position: 'absolute',
+                    bottom: 30,
+                    left: dimensions.width / 2 - 32,
+                    borderColor: primaryColor
+                }
+            ]}
+        >
+
+        </TouchableOpacity>
+    )
+}
+
+const FlipCameraButton = ({onPress = () => {}}) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.flipCameraButton}>
+            <Ionicons name='camera-reverse-outline' size={30} color='white'/>
+        </TouchableOpacity>
+    )
+}
+
+const GalleryButton = ({onPress = () => {}}) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.galleryButton}>
+            <Ionicons name='images-outline' size={30} color='white'/>
+        </TouchableOpacity>
+    )
+}
+
+const ReturnCancelButton = ({onPress = () => {}}) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.returnCancelButton}>
+            <Ionicons name='arrow-back-outline' size={30} color='white'/>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -72,5 +140,52 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+  },
+
+  shutterButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: 'white',
+    borderColor: 'red',
+    borderWidth: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  flipCameraButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 32,
+    backgroundColor: '#17202A',
+    position: 'absolute',
+    bottom: 40,
+    right: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  galleryButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 32,
+    backgroundColor: '#17202A',
+    position: 'absolute',
+    bottom: 40,
+    left: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  returnCancelButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 32,
+    backgroundColor: '#17202A',
+    position: 'absolute',
+    top: 40,
+    left: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
